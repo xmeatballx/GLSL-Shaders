882cae9aa6d78cbaa2d6a6e594b041215241e0be
xmeatballx <erik.jensen5@pcc.edu>
2020-11-23 03:33:11 -0800

    fixed scale jittering3

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 var canvas = new Array(document.getElementById('flowy'), document.getElementById('motion'), document.getElementById('mod'));
 var shaderlist = document.getElementById('shaderlist');
-var width = 500;
-canvas.width = width;
-canvas.height = width;
 
 var listValues = new Array("flow fields", "noise motion", "modulation station")
 
 setInterval(() => {
     for (var i = 0; i<3; i++){
-        canvas[i].width = width;
-        canvas[i].height = width;
         if (shaderlist.value != listValues[i]){
             canvas[i].classList.remove("visible");
             canvas[i].classList.add("invisible");
