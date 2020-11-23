79a6347ee841bdd2dd7d9ff1b0afa711f15da025
xmeatballx <erik.jensen5@pcc.edu>
2020-11-22 23:38:13 -0800

    reshow3

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var canvas = new Array(document.getElementById('flowy'), document.getElementById('motion'), document.getElementById('mod'));
 var shaderlist = document.getElementById('shaderlist');
+var width = 1000;
+canvas.width = width;
+canvas.height = width;
 
 var listValues = new Array("flow fields", "noise motion", "modulation station")
 
@@ -9,6 +12,8 @@ setInterval(() => {
             canvas[i].classList.remove("visible");
             canvas[i].classList.add("invisible");
         } else {
+            canvas.width = width;
+            canvas.height = width;
             canvas[i].classList.remove("invisible");
             canvas[i].classList.add("visible");
         }
